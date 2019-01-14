b7298d1ee213356fa6e2131ea120af55acca01aa
AlgoDesk07\AlgoUser <esp2ar0@gmail.com>
2019-01-14 19:02:11 +0900

    Add : User, UserController

---
+++ src/main/java/codesquad/controller/UserController.java
@@ -0,0 +1,26 @@
+package codesquad.controller;
+
+import codesquad.domain.user.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.*;
+
+@Controller
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/users")
+    public String create(User user) {
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String readUsers(Model model) {
+        model.addAttribute("users", users);
+        return "users/list";
+    }
+}
