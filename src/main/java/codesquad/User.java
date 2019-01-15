ae797586aa6fea159669b8d80359604f37324b9e
SimKiSeong <totokisung@naver.com>
2019-01-15 23:24:55 +0900

    feature : formerror

--- src/main/java/codesquad/User.java
+++ src/main/java/codesquad/User.java
@@ -1,4 +1,45 @@
 package codesquad;
 
 public class User {
+
+    String userId;
+    String userPassword;
+    String userEmail;
+    String userName;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
 }
--- src/main/java/codesquad/UserController.java
+++ src/main/java/codesquad/UserController.java
@@ -1,4 +1,56 @@
 package codesquad;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    List<User> userList = new ArrayList<User>();
+
+    @PostMapping(value="/user/form")
+    public String returnForm(Model model, HttpServletRequest request){
+
+        String userId = request.getParameter("userID");
+        String userPassword = request.getParameter("password");
+        String userName = request.getParameter("name");
+        String userEmail = request.getParameter("email");
+
+        User user = new User();
+        user.setUserId(userId);
+        user.setUserPassword(userPassword);
+        user.setUserName(userName);
+        user.setUserEmail(userEmail);
+
+        userList.add(user);
+
+        return "redirect:/users";
+    }
+
+
+/*
+    @GetMapping(value="/user/list")
+    public String userList(Model model){
+
+        return "user/list";
+    }
+
+    @GetMapping(value="/user/{userId}")
+    public String userModified(Model model){
+
+
+
+        return "user/list";
+    }
+
+*/
+
+
 }
