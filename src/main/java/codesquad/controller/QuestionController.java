256f3a6fc9ff56a076b6ebd5a64d6fcf19227a13
wijehyeon <xile803@gmail.com>
2019-01-29 01:51:36 +0900

    add : TODO List 1. Question : 4 TODO 2. QuestionController : 6 TODO

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//TODO 5. Use @Autowired annotation, make dependency relation with QuestionRepository and QuestionController
+//TODO 6. Save Question data via QuestionRepository's save() method.
+
 @Controller
 public class QuestionController {
 
@@ -26,12 +30,13 @@ public class QuestionController {
         return "redirect:/";
     }
 
+    //TODO 7. Use QuestionRepository's findAll() method, request the whole question list data.
     @GetMapping("/")
     public String bringQuestionsList(Model model) {
         model.addAttribute("questions", questions);
         return "qna/list";
     }
-
+    //TODO 8. As same as TODO 7, use findAll() method, and make show detail of question function.
     @GetMapping("/questions/{index}")
     public String showQuestion(Model model, @PathVariable int index) {
         for (int i = 0; i < questions.size(); i++) {
@@ -42,4 +47,11 @@ public class QuestionController {
         }
         return "qna/show";
     }
+
+    //TODO 9. Requirement : In the question detail showing view, it should make it possible update question.
+    // it should be done by edit qna/form.html file to qna/updateForm.html file and some change the details.
+
+    //TODO 10. Requirement : You can also delete question in the detail showing view.
+    // use QuestionRepository's delete() method. When in you click the delete button in show.html, delete value should transmitted\
+    // such as hidden like "<input type='hidden' name='_method' value='DELETE'/>"
 }
--- src/main/java/codesquad/model/Question.java
+++ src/main/java/codesquad/model/Question.java
@@ -1,5 +1,9 @@
 package codesquad.model;
 
+//TODO 1. use @Entity and map the Question class to Database Table
+//TODO 2. use @Id, @GeneratedValue annotation, make them as Question's key variable
+//TODO 3. use @Column annotation, mapping each table's field to table's column
+//TODO 4. Make QuestionRepository, this could extends CrudRepository
 public class Question {
     private int index;
     private String writer;
