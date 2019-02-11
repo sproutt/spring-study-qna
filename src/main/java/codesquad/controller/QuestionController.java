87cab14f960601ef30bc4b61d99aee9ad3b35846
SimKiSeong <totokisung@naver.com>
2019-02-11 12:05:56 +0900

    fix : Put Delete매핑관련 url 수정

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -4,6 +4,7 @@ import codesquad.model.question.Question;
 import codesquad.model.question.QuestionRepository;
 import codesquad.model.user.User;
 import codesquad.model.user.UserRepository;
+import codesquad.utils.sessionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,7 +25,7 @@ public class QuestionController {
 
     @GetMapping("/qna/form")
     public String returnQnaForm(HttpSession session){
-        Optional<User> user = (Optional<User>) session.getAttribute("user");
+        Optional<User> user = sessionUtil.getSessionUser(session);
         if(user==null){
             return"redirect:/users/loginForm";
         }
@@ -46,7 +47,7 @@ public class QuestionController {
         return"redirect:/";
     }
 
-    @PutMapping("/question/update/{id}")
+    @PutMapping("/question/{id}")
     public String updateQuestion(Model model, @PathVariable Long id, HttpSession session)throws Exception{
         User user = (User)session.getAttribute("user");
         if(user == null){
@@ -61,7 +62,7 @@ public class QuestionController {
         return"/qna/updateForm";
     }
 
-    @DeleteMapping("/question/delete/{id}")
+    @DeleteMapping("/question/{id}")
     public String deleteQuestion(Model model, @PathVariable Long id,HttpSession session)throws Exception{
         User user = (User)session.getAttribute("user");
         if(user == null){
