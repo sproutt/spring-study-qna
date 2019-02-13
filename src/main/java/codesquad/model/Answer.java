4055df862b91aef4ced661d64a4a2e5dbe3aa7eb
wijehyeon <xile803@gmail.com>
2019-02-13 13:40:19 +0900

    add : TODO List추가

---
+++ src/main/java/codesquad/model/Answer.java
@@ -0,0 +1,12 @@
+package codesquad.model;
+
+public class Answer {
+    //TODO 1. Answer엔티티 작성하고 DB와 매핑시키기
+    //TODO 2. Question와 1:N 관계
+    //TODO 3. AnswerRepository작성, findByQuestionId
+    //TODO 4. Answer Controller추가
+    //        /questions/{questionId}/answers/{id}
+
+
+
+}
