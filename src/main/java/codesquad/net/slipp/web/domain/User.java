36dbda166b334fea50735da313097d662a84d06d
dadadamarine <dadadamarine@naver.com>
2019-02-13 15:09:40 +0900

    add : lombok 추가

--- src/main/java/codesquad/net/slipp/web/domain/User.java
+++ src/main/java/codesquad/net/slipp/web/domain/User.java
@@ -1,5 +1,8 @@
 package codesquad.net.slipp.web.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,70 +13,24 @@ public class User {
 
     @Id
     @GeneratedValue
+    @Getter
     private Long id;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(30)")
+    @Getter @Setter
     private String userId;
 
     @Column(nullable = false, columnDefinition = "CHAR(64)")
+    @Getter @Setter
     private String password;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    @Getter @Setter
     private String name;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(50)")
+    @Getter @Setter
     private String email;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                "userId='" + userId + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 
     public boolean match(User user) {
 
