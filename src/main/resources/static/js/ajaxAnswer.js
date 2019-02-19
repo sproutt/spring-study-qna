670c9e9742cceaf5dcff6c345c9ac5fda9176559
dadadamarine <dadadamarine@naver.com>
2019-02-20 04:06:54 +0900

    add : ajax 삭제기능 완성

--- src/main/resources/static/js/ajaxAnswer.js
+++ src/main/resources/static/js/ajaxAnswer.js
@@ -49,28 +49,28 @@ function registerAnswerHandler(event) {
 }
 
 
-function appendAnswer({id, question, writer, contents, createYearToSecond}) {
+function appendAnswer({result, data}) {
     const html = `
-        <article class="article">
+        <article class="article" id="answer-${data.id}">
             <div class="article-header">
                 <div class="article-header-thumb">
                     <img src="https://graph.facebook.com/v2.3/1324855987/picture" class="article-author-thumb" alt="">
                 </div>
                 <div class="article-header-text">
-                    <a href="#" class="article-author-name">${writer.name}</a>
-                    <div class="article-header-time">${createYearToSecond}</div>
+                    <a href="#" class="article-author-name">${data.writer.name}</a>
+                    <div class="article-header-time">${data.createYearToSecond}</div>
                 </div>
             </div>
             <div class="article-doc comment-doc">
-                <p>${contents}</p>
+                <p>${data.contents}</p>
             </div>
             <div class="article-util">
             <ul class="article-util-list">
                 <li>
-                    <a class="link-modify-article" href="/api/questions/${question.id}/answers/${id}">수정</a>
+                    <a class="link-modify-article" href="/api/questions/${data.question.id}/answers/${data.id}">수정</a>
                 </li>
                 <li>
-                    <form class="delete-answer-form" action="/api/questions/${question.id}/answers/${id}" method="POST">
+                    <form class="delete-answer-form" action="/api/questions/${data.question.id}/answers/${data.id}" method="POST">
                         <input type="hidden" name="_method" value="DELETE">
                         <button type="submit" class="delete-answer-button">삭제</button>
                     </form>
@@ -79,7 +79,7 @@ function appendAnswer({id, question, writer, contents, createYearToSecond}) {
             </div>
         </article> `
 
-    $(".qna-comment-count strong").innerText = `${question.size}`;
+    $(".qna-comment-count strong").innerText = `${data.question.size}`;
     $(".qna-comment-slipp-articles").insertAdjacentHTML("afterbegin", html);
 }
 
@@ -93,8 +93,8 @@ function deleteAnswerHandler(event){
         callback: deleteAnswer
     })
 }
-function deleteAnswer({a}){
-    const query = `.article[id="answer-"${answerid}]`;
+function deleteAnswer({result, data}){
+    const query = `.article[id="answer-${data}"]`;
     console.log(query);
     const target = $(query);
     target.parentNode.removeChild(target);
