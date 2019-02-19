a679d82797ab5859dc2db3d349bd5fa85ebcc0a0
wijehyeon <xile803@gmail.com>
2019-02-19 21:40:36 +0900

    refactor : qna.js

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -3,7 +3,6 @@ package codesquad.controller;
 import codesquad.model.Question;
 import codesquad.model.User;
 import codesquad.service.QuestionService;
-import codesquad.service.UserService;
 import codesquad.utils.SessionChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
