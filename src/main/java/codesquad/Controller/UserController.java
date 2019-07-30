089f28aa559d43d78790161bb7a00f7120954966
jbj616 <jbj616@gmail.com>
2019-07-30 12:05:38 +0900

    add : 회원목록 기능 구현

--- src/main/java/codesquad/Controller/UserController.java
+++ src/main/java/codesquad/Controller/UserController.java
@@ -3,25 +3,30 @@ package codesquad.Controller;
 import codesquad.Model.User;
 import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-  ArrayList<User> users = new ArrayList<>();
+  private ArrayList<User> users = new ArrayList<>();
 
-  @PostMapping("/users")
+  @PostMapping("/user/signup")
   public String users(User user){
 
     users.add(user);
+    System.out.println("user 추가됨. =>" + users.toString());
 
     return "redirect:/users";
   }
 
-  @GetMapping("/users")
-  public String user(){
+  @GetMapping("/user/list")
+  public String user(Model model){
 
-    return "userlist";
+    model.addAttribute("users", users);
+    System.out.println("users 뷰로 전송됨.");
+
+    return "user/list";
   }
 }
