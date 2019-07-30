8e35c5ca4b0f12a0989418e6938ab97fd892c413
jbj616 <jbj616@gmail.com>
2019-07-30 13:15:36 +0900

    add : 회원 프로필 정보보기

--- src/main/java/codesquad/Controller/UserController.java
+++ src/main/java/codesquad/Controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -13,20 +14,37 @@ public class UserController {
   private ArrayList<User> users = new ArrayList<>();
 
   @PostMapping("/user/signup")
-  public String users(User user){
+  public String createUser(User user){
 
     users.add(user);
     System.out.println("user 추가됨. =>" + users.toString());
 
-    return "redirect:/users";
+    return "redirect:/user/list";
   }
 
   @GetMapping("/user/list")
-  public String user(Model model){
+  public String showUserList(Model model){
 
     model.addAttribute("users", users);
     System.out.println("users 뷰로 전송됨.");
 
     return "user/list";
+  }
+
+  @GetMapping("/user/profile/{userId}")
+  public String showProfile(@PathVariable("userId") String userId, Model model){
+    System.out.println(" userId 전달됨. -> "+ userId);
+
+    model.addAttribute("user", findUser(userId));
+    System.out.println("user 뷰로 전송됨.");
+
+    return "user/profile";
+  }
+
+  private User findUser(String userId){
+
+    return users.stream().filter((user -> user.getUserId().equals(userId)))
+        .findFirst()
+        .get();
   }
 }
