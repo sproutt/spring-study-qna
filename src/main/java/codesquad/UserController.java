0c877ee1ead04bbe4b3f18e25e56042045e70c14
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-08-14 01:03:53 +0900

    feat : signup

---
+++ src/main/java/codesquad/UserController.java
@@ -0,0 +1,26 @@
+package codesquad;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<User>();
+    @PostMapping("/users")
+    public String create(User user){
+        System.out.println("user : " + user);
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model){
+        model.addAttribute("users",users);
+        return "users";
+    }
+}
