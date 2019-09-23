0abf8969f27fefd5a108afdd799e8b0d616459e8
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-09-23 23:40:57 +0900

    질문목록 요구사항

--- src/main/java/codesquad/Question.java
+++ src/main/java/codesquad/Question.java
@@ -4,6 +4,24 @@ public class Question {
     private String writer;
     private String title;
     private String contents;
+    private String time;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
     public String getWriter() {
         return writer;
@@ -35,6 +53,8 @@ public class Question {
                 "writer='" + writer + '\'' +
                 ", title='" + title + '\'' +
                 ", contents='" + contents + '\'' +
+                ", time='" + time + '\'' +
+                ", index=" + index +
                 '}';
     }
 }
--- src/main/java/codesquad/QuestionController.java
+++ src/main/java/codesquad/QuestionController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -19,15 +21,24 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String question(Question question) {
-        System.out.println("question : "+ question);
+        question.setIndex(questionList.size() + 1);
+        String time = checkCurrentTime();
+        question.setTime(time);
         questionList.add(question);
+        System.out.println("add 후 question : " + question);
         return "redirect:/";
     }
 
     @GetMapping("/")
-    public String questionList(Model model){
+    public String questionList(Model model) {
         model.addAttribute("questionList", questionList);
         return "index";
     }
 
+    private String checkCurrentTime() {
+        Date today = new Date();
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
+        return date.format(today)+" "+time.format(today);
+    }
 }
