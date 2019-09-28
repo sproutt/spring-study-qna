87528431dd08a2b924974293b1b26820ad3dfaf0
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-09-28 14:04:58 +0900

    feat : DB에 회원 데이터 저장

--- src/main/java/codesquad/UserController.java
+++ src/main/java/codesquad/UserController.java
@@ -1,45 +1,41 @@
 package codesquad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList<User>();
 
-    @PostMapping("/users")
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("")
     public String create(User user) {
         System.out.println("user : " + user);
-        user.setUserIndex(users.size() + 1);
-        System.out.println("user : " + user);
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", users);
-        return "users";
+        model.addAttribute("users", userRepository.findAll());
+        return "/user/list";
     }
 
-    @GetMapping("/users/{userId}")
-    public String getProfile(@PathVariable("userId") String userId, Model model) {
-
-        for (User user : users) {
-            if (user.isSameUser(userId, user)) {
-                model.addAttribute("user", user);
-                break;
-            }
-        }
-        return "profile";
+    @GetMapping("/{id}")
+    public ModelAndView show(@PathVariable long id) {
+        ModelAndView mav = new ModelAndView("user/profile");
+        mav.addObject("user", userRepository.findById(id).get());
+        return mav;
     }
-
+    /*
     @GetMapping("/users/{userId}/form")
     public String updateForm(@PathVariable("userId") String userId, Model model) {
         for (User user : users) {
@@ -68,4 +64,5 @@ public class UserController {
         }
         return "redirect:/users";
     }
+    */
 }
