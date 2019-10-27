027c953bd511be82835678a01e4b166c9caf0f31
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-10-28 04:05:18 +0900

    chore

--- src/main/java/codesquad/controller/AnswerController.java
+++ src/main/java/codesquad/controller/AnswerController.java
@@ -7,14 +7,10 @@ import codesquad.repository.AnswerRepository;
 import codesquad.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.naming.directory.NoSuchAttributeException;
-import javax.persistence.GeneratedValue;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.NoSuchElementException;
