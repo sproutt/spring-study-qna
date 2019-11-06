a014e0e212c32977f59e742d9ac5ad282a32cb3b
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-11-06 16:37:59 +0900

    feat : userService

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -2,6 +2,7 @@ package codesquad.controller;
 
 import codesquad.domain.User;
 import codesquad.repository.UserRepository;
+import codesquad.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,81 +14,45 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping("")
     public String create(User user) {
-        userRepository.save(user);
+        userService.createUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findUsers());
         return "user/list";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(NullPointerException::new);
-        model.addAttribute("user", user);
+        model.addAttribute("user", userService.findUser(id));
         return "user/profile";
     }
 
     @GetMapping("/{id}/form")
     public String updateForm(@PathVariable("id") Long id, Model model, HttpSession session) {
-        userRepository.findById(id).orElseThrow(NullPointerException::new);
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
-            User user = (User) value;
-            if (id.equals(user.getId())) {
-                model.addAttribute("user", user);
-                return "user/updateForm";
-            } else {
-                model.addAttribute("userMissMatch", userRepository.findAll());
-                return "user/list";
-            }
-        } else {
-            return "user/login";
-        }
+        return userService.userUpdate(id, model,session);
     }
 
     @PutMapping("/{id}/update")
     public String editUser(@PathVariable("id") Long id, User user, HttpSession session) {
-        userRepository.findById(id).orElseThrow(NullPointerException::new);
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
-            User originUser = (User) value;
-            if (originUser.isSamePassword(user.getPassword())) {
-                originUser.editProfile(user);
-                userRepository.save(originUser);
-                return "redirect:/users";
-            } else {
-                return "user/updateForm";
-            }
-        } else {
-            return "user/login";
-        }
+        return  userService.userEdit(id, user, session);
     }
 
     @PostMapping("/doLogin")
     public String loginAccess(String userId, String password, HttpSession session) {
-        Optional<User> maybeUser = userRepository.findByUserId(userId);
-        User user = maybeUser.orElseThrow(NullPointerException::new);
-        if (user.isSamePassword(password)) {
-            session.setAttribute("sessionedUser", user);
-            return "redirect:/users";
-        } else {
-            return "user/login_failed";
-        }
+        return userService.userLogin(userId,password,session);
     }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        session.removeAttribute("seesionedUser");
-        session.invalidate();
+        userService.userLogout(session);
         return "redirect:/";
     }
 }
---
+++ src/main/java/codesquad/service/UserService.java
@@ -0,0 +1,81 @@
+package codesquad.service;
+
+import codesquad.domain.User;
+import codesquad.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public User findUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(NullPointerException::new);
+        return user;
+    }
+
+    public String userLogin(String userId, String password, HttpSession session) {
+        Optional<User> maybeUser = userRepository.findByUserId(userId);
+        User user = maybeUser.orElseThrow(NullPointerException::new);
+        if (user.isSamePassword(password)) {
+            session.setAttribute("sessionedUser", user);
+            return "redirect:/users";
+        } else {
+            return "user/login_failed";
+        }
+    }
+
+    public void userLogout(HttpSession session) {
+        session.removeAttribute("seesionedUser");
+        session.invalidate();
+    }
+
+    public String userUpdate(Long id, Model model, HttpSession session) {
+        userRepository.findById(id).orElseThrow(NullPointerException::new);
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null) {
+            User user = (User) value;
+            if (id.equals(user.getId())) {
+                model.addAttribute("user", user);
+                return "user/updateForm";
+            } else {
+                model.addAttribute("userMissMatch", userRepository.findAll());
+                return "user/list";
+            }
+        } else {
+            return "user/login";
+        }
+    }
+
+    public String userEdit(Long id, User user, HttpSession session) {
+        userRepository.findById(id).orElseThrow(NullPointerException::new);
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null) {
+            User originUser = (User) value;
+            if (originUser.isSamePassword(user.getPassword())) {
+                originUser.editProfile(user);
+                userRepository.save(originUser);
+                return "redirect:/users";
+            } else {
+                return "user/updateForm";
+            }
+        } else {
+            return "user/login";
+        }
+
+    }
+}
