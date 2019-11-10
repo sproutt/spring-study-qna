c7a8d6f01b0d586ad413c56152b346fd67b1261c
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-11-10 20:23:57 +0900

    feat : questionService

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -1,112 +1,54 @@
 package codesquad.controller;
 
-import codesquad.domain.Answer;
 import codesquad.domain.Question;
-import codesquad.domain.User;
-import codesquad.repository.AnswerRepository;
-import codesquad.repository.QuestionRepository;
+import codesquad.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @Controller
 public class QuestionController {
-    @Autowired
-    private QuestionRepository questionRepository;
-    @Autowired
-    private AnswerRepository answerRepository;
 
+    @Autowired
+    private QuestionService questionService;
 
     @PostMapping("/questions")
     public String question(Question question, HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
-            User loginUser = (User) value;
-            question.setCurrentTime();
-            question.setUserInfo(loginUser);
-            questionRepository.save(question);
-            System.out.println("add 후 question : " + question);
-            return "redirect:/";
-        } else {
-            return "redirect:/users/login";
-        }
+        return questionService.question(question, session);
     }
 
     @GetMapping("/")
     public String questionList(Model model) {
-        model.addAttribute("questionList", questionRepository.findAll());
+        model.addAttribute("questionList", questionService.findQuestionList());
         return "index";
     }
 
     @GetMapping("/questions/{id}")
     public String questionsShow(@PathVariable("id") Long id, Model model) {
-        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        model.addAttribute("question", question);
+        model.addAttribute("question", questionService.findQuestion(id));
         return "qna/show";
     }
 
     @GetMapping("/questions/{id}/form")
     public String updateQuestionForm(@PathVariable("id") Long id, Model model, HttpSession session) {
-        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null && question.getWriter().isSameUserId((User) value)) {
-            model.addAttribute("question", question);
-            return "qna/updateForm";
-        } else {
-            model.addAttribute("userMissMatchQuestion", question);
-            return "qna/show";
-        }
+        return questionService.updateQuestion(id, model, session);
     }
 
     @PutMapping("/questions/{id}")
     public String editQuestion(@PathVariable("id") Long id, Question newQuestion, Model model, HttpSession session) {
-        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null && question.getWriter().isSameUserId((User) value)) {
-            question.changeInfo(newQuestion);
-            questionRepository.save(question);
-            return "redirect:/";
-        } else {
-            model.addAttribute("userMissMatchQuestion", question);
-            return "qna/show";
-        }
+        return questionService.editQuestion(id, newQuestion, model, session);
     }
 
     @DeleteMapping("/questions/{id}")
     public String deleteQuestion(@PathVariable("id") Long id, HttpSession session) {
-        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null && question.getWriter().isSameUserId((User) value)) {
-            if (answerRepository.findByQuestionId(id).isPresent()) {
-                List<Answer> answers = answerRepository.findByQuestionId(id).orElseThrow(NullPointerException::new);
-                for (int i = 0; i < answers.size(); i++) {
-                    if(!answers.get(i).getWriterId().equals(question.getWriter().getId())){
-                        return "redirect:/questions/"+id;
-                    }
-                }
-                questionRepository.delete(question);
-                return "redirect:/";
-            } else {
-                questionRepository.delete(question);
-                return "redirect:/";
-            }
-        } else {
-            return "user/login_failed";
-        }
+        return questionService.deleteQuestion(id, session);
     }
 
     @GetMapping("/questions/form")
-    public String doQuestion(HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
-            return "qna/form";
-        } else {
-            return "/users/login";
-        }
+    public String clickQuestion(HttpSession session) {
+        return questionService.clickQuestion(session);
     }
 }
--- src/main/java/codesquad/service/QuestionService.java
+++ src/main/java/codesquad/service/QuestionService.java
@@ -1,4 +1,101 @@
 package codesquad.service;
 
+import codesquad.domain.Answer;
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.repository.AnswerRepository;
+import codesquad.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
 public class QuestionService {
+    @Autowired
+    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public List<Question> findQuestionList() {
+        return questionRepository.findAll();
+    }
+
+    public Question findQuestion(Long id) {
+        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        return question;
+    }
+
+    public String updateQuestion(Long id, Model model, HttpSession session) {
+        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null && question.getWriter().isSameUserId((User) value)) {
+            model.addAttribute("question", question);
+            return "qna/updateForm";
+        } else {
+            model.addAttribute("userMissMatchQuestion", question);
+            return "qna/show";
+        }
+    }
+
+    public String editQuestion(Long id, Question newQuestion, Model model, HttpSession session) {
+        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null && question.getWriter().isSameUserId((User) value)) {
+            question.changeInfo(newQuestion);
+            questionRepository.save(question);
+            return "redirect:/";
+        } else {
+            model.addAttribute("userMissMatchQuestion", question);
+            return "qna/show";
+        }
+    }
+
+    public String deleteQuestion(Long id, HttpSession session) {
+        Question question = questionRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null && question.getWriter().isSameUserId((User) value)) {
+            if (answerRepository.findByQuestionId(id).isPresent()) {
+                List<Answer> answers = answerRepository.findByQuestionId(id).orElseThrow(NullPointerException::new);
+                for (int i = 0; i < answers.size(); i++) {
+                    if (!answers.get(i).getWriterId().equals(question.getWriter().getId())) {
+                        return "redirect:/questions/" + id;
+                    }
+                }
+                questionRepository.delete(question);
+                return "redirect:/";
+            } else {
+                questionRepository.delete(question);
+                return "redirect:/";
+            }
+        } else {
+            return "user/login_failed";
+        }
+    }
+
+    public String clickQuestion(HttpSession session) {
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null) {
+            return "qna/form";
+        } else {
+            return "/users/login";
+        }
+    }
+
+    public String question(Question question, HttpSession session) {
+        Object value = session.getAttribute("sessionedUser");
+        if (value != null) {
+            User loginUser = (User) value;
+            question.setCurrentTime();
+            question.setUserInfo(loginUser);
+            questionRepository.save(question);
+            System.out.println("add 후 question : " + question);
+            return "redirect:/";
+        } else {
+            return "redirect:/users/login";
+        }
+    }
 }
