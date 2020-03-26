b458775c18439764814b3e65db3e50c19e45aa58
Ilisha29 <94konzern@gmail.com>
2020-03-26 22:14:18 +0900

    add : 회원가입 기능 / fix : html 파일 옮기기 / add : base.html

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -2,19 +2,32 @@ package codesquad.controller;
 
 import codesquad.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
-    @GetMapping("user/form")
+    private List<User> users = new ArrayList<User>();
+
+    @GetMapping("/user/form")
     public String showForm() {
         return "user/form";
     }
 
-    @PostMapping("/users")
+    @PostMapping("/user/create")
     public String create(User user) {
         System.out.println("user : " + user);
-        return "redirect:/users";
+        users.add(user);
+        return "redirect:/users/list";
+    }
+
+    @GetMapping("/users/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "user/list";
     }
 }
