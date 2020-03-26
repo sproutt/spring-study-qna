dd38ccdbf3d1a28b6cd6499e1eb3e86dcb78be75
Ilisha29 <94konzern@gmail.com>
2020-03-26 23:08:08 +0900

    add : 회원 프로필 정보보기

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -4,6 +4,7 @@ import codesquad.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Controller
 public class UserController {
-    private List<User> users = new ArrayList<User>();
+    private List<User> users = new ArrayList<>();
 
     @GetMapping("/user/form")
     public String showForm() {
@@ -20,14 +21,29 @@ public class UserController {
 
     @PostMapping("/user/create")
     public String create(User user) {
-        System.out.println("user : " + user);
         users.add(user);
-        return "redirect:/users/list";
+        return "redirect:/user/list";
     }
 
-    @GetMapping("/users/list")
+    @GetMapping("/user/list")
     public String list(Model model) {
         model.addAttribute("users", users);
         return "user/list";
     }
+
+    @GetMapping("/user/{userId}")
+    public String showProfile(@PathVariable String userId, Model model) {
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            if (findUser(user, userId)) {
+                model.addAttribute("user", user);
+            }
+        }
+        return "user/profile";
+    }
+
+    private boolean findUser(User user, String userId) {
+        return user.getUserId().equals(userId);
+    }
+
 }
