b8650da04c81dc5af4c398428477b89465c9d896
Ilisha29 <94konzern@gmail.com>
2020-04-09 15:43:25 +0900

    add : 질문 데이터를 DB에 저장 및 조회

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package codesquad.controller;
 
 import codesquad.domain.Question;
+import codesquad.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,13 @@ import java.util.List;
 
 @Controller
 public class QuestionController {
-    private List<Question> questionList = new ArrayList<>();
+
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @GetMapping("/")
     public String get(Model model) {
-        model.addAttribute("questionList", questionList);
+        model.addAttribute("questionList", questionRepository.findAll());
         return "qna/index";
     }
 
@@ -27,13 +31,13 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public String create(Question question) {
-        questionList.add(question);
+        questionRepository.save(question);
         return "redirect:/";
     }
 
-    @GetMapping("/questions/{index}")
-    public String get(@PathVariable int index, Model model) {
-        model.addAttribute("question", questionList.get(index));
+    @GetMapping("/questions/{id}")
+    public String get(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id));
         return "qna/show";
     }
 }
--- src/main/java/codesquad/domain/Question.java
+++ src/main/java/codesquad/domain/Question.java
@@ -1,9 +1,23 @@
 package codesquad.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
     private String writer;
+
     private String title;
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getWriter() {
         return writer;
@@ -17,6 +31,10 @@ public class Question {
         return title;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -28,4 +46,5 @@ public class Question {
     public void setContents(String contents) {
         this.contents = contents;
     }
+
 }
