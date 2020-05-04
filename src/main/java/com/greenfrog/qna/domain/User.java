6cbbd38a33e530e0372551899cf0bcfb23d520c4
배종진 <qowhdwlsdk@gmail.com>
2020-05-04 11:34:28 +0900

    feat: GeneratedValue 전략 추가

--- src/main/java/com/greenfrog/qna/domain/User.java
+++ src/main/java/com/greenfrog/qna/domain/User.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userId;
     private String password;
