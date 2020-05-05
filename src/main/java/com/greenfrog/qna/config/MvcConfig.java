4b93538218f91c0b7cbc39cdb2ed3793edb8d83a
배종진 <qowhdwlsdk@gmail.com>
2020-05-05 21:20:46 +0900

    Refctor: 불필요한 import문 삭제

--- src/main/java/com/greenfrog/qna/config/MvcConfig.java
+++ src/main/java/com/greenfrog/qna/config/MvcConfig.java
@@ -1,7 +1,6 @@
 
 package com.greenfrog.qna.config;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
