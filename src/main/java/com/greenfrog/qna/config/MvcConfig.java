0d94111b19530a5cec6ce6acceebb2a492ecd6b2
배종진 <qowhdwlsdk@gmail.com>
2020-05-05 14:41:48 +0900

    Refator: MvcConfig 클래스에서 WebMvcConfigurer 인터페이스를 구현하도록 리팩토링

--- src/main/java/com/greenfrog/qna/config/MvcConfig.java
+++ src/main/java/com/greenfrog/qna/config/MvcConfig.java
@@ -1,19 +1,20 @@
 
 package com.greenfrog.qna.config;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig extends WebMvcConfigurationSupport {
+public class MvcConfig implements WebMvcConfigurer {
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
