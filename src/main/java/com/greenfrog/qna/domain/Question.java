88f227a04acf4e8560a52a45f2ca7dccfd60d534
배종진 <qowhdwlsdk@gmail.com>
2020-05-09 17:23:54 +0900

    Refactor: Question 클래스 설계 변경

--- src/main/java/com/greenfrog/qna/domain/Question.java
+++ src/main/java/com/greenfrog/qna/domain/Question.java
@@ -1,11 +1,9 @@
 package com.greenfrog.qna.domain;
 
 import com.greenfrog.qna.dto.QuestionUpdateDTO;
-import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Getter
 @Entity
@@ -19,31 +17,10 @@ public class Question {
     private String contents;
     private String createTime;
 
-    public Question() {
-
-    }
-
-    public Question(String writer, String title, String contents) {
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-    }
-
-
     public void update(QuestionUpdateDTO questionUpdateDTO) {
         this.writer = questionUpdateDTO.getWriter();
         this.title = questionUpdateDTO.getTitle();
         this.contents = questionUpdateDTO.getContents();
     }
 
-    @Override
-    public String toString() {
-        return "Question{" +
-                "id=" + id +
-                ", writer='" + writer + '\'' +
-                ", title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                ", createTime='" + createTime + '\'' +
-                '}';
-    }
 }
