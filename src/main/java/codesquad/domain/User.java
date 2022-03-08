f6ec073e4183698a27a8c35094d4d3baeef8820b
user <gusdn85554@naver.com>
2022-03-09 02:00:09 +0900

    feat : User, UserRepository, UserRepositoryTest 구현
    User 도메인 생성
    UserRepository 순수 JPA 사용하여 회원정보 저장, 모든 회원 조회 기능 구현
    UserRepositoryTest를 통해 UserRepository 테스트

---
+++ src/main/java/codesquad/domain/User.java
@@ -0,0 +1,64 @@
+package codesquad.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String userId;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/codesquad/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package codesquad.repository;
+
+import codesquad.domain.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private final EntityManager em;
+
+    public UserRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Long save(User user) {
+        em.persist(user);
+        return user.getId();
+    }
+
+    public List<User> findAll() {
+        return em.createQuery(
+                "select u from User u", User.class)
+                .getResultList();
+    }
+}
