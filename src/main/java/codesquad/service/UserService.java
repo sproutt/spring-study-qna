9ebefd070a5584727a5effb5e15dfcd29e55c252
saint6839 <saint6839@gmail.com>
2022-03-09 21:22:15 +0900

    feat: userId로 사용자 조회 메서드 추가

--- src/main/java/codesquad/service/UserService.java
+++ src/main/java/codesquad/service/UserService.java
@@ -4,6 +4,7 @@ import codesquad.domain.User;
 import codesquad.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -20,15 +21,22 @@ public class UserService {
 
     private void validateDuplicateUser(User user) {
         userRepository.findByUserId(user.getUserId())
-                .ifPresent(m -> { throw new IllegalArgumentException("이미 사용중인 사용자 ID 입니다");
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 사용중인 사용자 ID 입니다");
                 });
 
         userRepository.findByEmail(user.getEmail())
-                .ifPresent(m -> { throw new IllegalArgumentException("이미 사용중인 이메일 주소 입니다.");
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 사용중인 이메일 주소 입니다.");
                 });
     }
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findByUserId(String userId) {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new NoSuchElementException("해당 사용자 아이디와 일치하는 사용자가 없습니다."));
     }
 }
