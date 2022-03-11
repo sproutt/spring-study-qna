c33994dfaa1e80877a9412b1661dace4b4d97622
saint6839 <saint6839@gmail.com>
2022-03-11 22:04:56 +0900

    refactor: 예외처리 메시지 상수화

--- src/main/java/codesquad/service/UserService.java
+++ src/main/java/codesquad/service/UserService.java
@@ -8,6 +8,10 @@ import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
+    private static final String ALREADY_EXIST_USER_ID_ERROR = "이미 사용중인 사용자 ID 입니다";
+    private static final String ALREADY_EXIST_EMAIL_ERROR = "이미 사용중인 사용자 이메일 주소 입니다";
+    private static final String NOT_MATCH_EXIST_USER_ID_ERROR = "해당 사용자 아이디와 일치하는 사용자가 없습니다.";
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -22,12 +26,12 @@ public class UserService {
     private void validateDuplicateUser(User user) {
         userRepository.findByUserId(user.getUserId())
                 .ifPresent(m -> {
-                    throw new IllegalArgumentException("이미 사용중인 사용자 ID 입니다");
+                    throw new IllegalArgumentException(ALREADY_EXIST_USER_ID_ERROR);
                 });
 
         userRepository.findByEmail(user.getEmail())
                 .ifPresent(m -> {
-                    throw new IllegalArgumentException("이미 사용중인 이메일 주소 입니다.");
+                    throw new IllegalArgumentException(ALREADY_EXIST_EMAIL_ERROR);
                 });
     }
 
@@ -37,6 +41,6 @@ public class UserService {
 
     public User findByUserId(String userId) {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new NoSuchElementException("해당 사용자 아이디와 일치하는 사용자가 없습니다."));
+                .orElseThrow(() -> new NoSuchElementException(NOT_MATCH_EXIST_USER_ID_ERROR));
     }
 }
