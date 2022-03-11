14736a2718905c9728ad5ecf6481c5301d66f26b
saint6839 <saint6839@gmail.com>
2022-03-11 23:09:47 +0900

    refactor: 기존 예외 클래스 커스텀 예외 클래스로 변경

--- src/main/java/codesquad/service/UserService.java
+++ src/main/java/codesquad/service/UserService.java
@@ -1,16 +1,17 @@
 package codesquad.service;
 
 import codesquad.domain.User;
+import codesquad.exception.ExistEmailException;
+import codesquad.exception.ExistUserIdException;
+import codesquad.exception.NoSuchUserException;
 import codesquad.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
-    private static final String ALREADY_EXIST_USER_ID_ERROR = "이미 사용중인 사용자 ID 입니다";
-    private static final String ALREADY_EXIST_EMAIL_ERROR = "이미 사용중인 사용자 이메일 주소 입니다";
-    private static final String NOT_MATCH_EXIST_USER_ID_ERROR = "해당 사용자 아이디와 일치하는 사용자가 없습니다.";
 
     private final UserRepository userRepository;
 
@@ -26,12 +27,12 @@ public class UserService {
     private void validateDuplicateUser(User user) {
         userRepository.findByUserId(user.getUserId())
                 .ifPresent(m -> {
-                    throw new IllegalArgumentException(ALREADY_EXIST_USER_ID_ERROR);
+                    throw new ExistUserIdException();
                 });
 
         userRepository.findByEmail(user.getEmail())
                 .ifPresent(m -> {
-                    throw new IllegalArgumentException(ALREADY_EXIST_EMAIL_ERROR);
+                    throw new ExistEmailException();
                 });
     }
 
@@ -41,6 +42,6 @@ public class UserService {
 
     public User findByUserId(String userId) {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new NoSuchElementException(NOT_MATCH_EXIST_USER_ID_ERROR));
+                .orElseThrow(NoSuchUserException::new);
     }
 }
