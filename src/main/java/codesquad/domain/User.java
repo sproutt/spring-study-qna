6bffe5fdf33bd8a849c2685034de0bd9c03aa79a
saint6839 <saint6839@gmail.com>
2022-03-13 20:13:29 +0900

    refactor: 생성자 접근제한자 private로 변경

--- src/main/java/codesquad/domain/User.java
+++ src/main/java/codesquad/domain/User.java
@@ -16,14 +16,10 @@ public class User {
     private String name;
     private String email;
 
-    public User(String userId, String password, String name, String email) {
-        this.userId = userId;
-        this.password = password;
-        this.name = name;
-        this.email = email;
+    protected User() {
     }
 
-    protected User() {
+    private User(String userId, String password, String name, String email) {
     }
 
     public static User craeteUser(String userId, String password, String name, String email) {
