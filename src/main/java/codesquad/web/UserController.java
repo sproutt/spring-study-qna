71f81338d69f8bbd62462f510664a4b219fb0267
sunghyuki <sunghyuk1609@gmail.com>
2022-03-14 01:21:15 +0900

    feat : 회원 프로필 정보보기 기능 추가 - 사용자 아이디 링크 추가 - profile.html 추가 - 회원 프로필 GET 요청 메서드 추가

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -4,6 +4,7 @@ import codesquad.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -24,5 +25,16 @@ public class UserController {
     public String list(Model model) {
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        for (User user : users) {
+            if(user.getUserId().equals(userId)) {
+                model.addAttribute(user);
+                break;
+            }
+        }
+        return "user/profile";
     }
 }
