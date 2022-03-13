955fe0d1fd79873808a934227121984ece7f2b0b
saint6839 <saint6839@gmail.com>
2022-03-13 23:33:55 +0900

    refactor: 요구 범위에 맞추어서 DB없이 구현 되도록 수정

--- src/main/java/codesquad/controller/UserController.java
+++ src/main/java/codesquad/controller/UserController.java
@@ -1,7 +1,7 @@
 package codesquad.controller;
 
 import codesquad.domain.User;
-import codesquad.service.UserService;
+import codesquad.exception.NoSuchUserException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,35 +9,33 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
 public class UserController {
 
-    private final UserService userService;
+    private List<User> users = new ArrayList<>();
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-
-    @PostMapping("/user/create")
+    @PostMapping("/users")
     public String create(@ModelAttribute User user) {
-        userService.join(user);
+        users.add(user);
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String showAllUsers(Model model) {
-        List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "user/list";
     }
 
     @GetMapping("/users/{userId}")
     public String showUserInfo(@PathVariable String userId, Model model) {
-        User user = userService.findByUserId(userId);
+        Optional<User> userOptional = users.stream().filter(user -> user.getUserId().equals(userId)).findAny();
+        User user = userOptional.orElseThrow(NoSuchUserException::new);
+
         model.addAttribute("userId", user.getUserId());
         model.addAttribute("email", user.getEmail());
         return "user/profile";
