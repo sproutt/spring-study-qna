d77543c124cfc656b7caf306ececa11e55078733
user <gusdn85554@naver.com>
2022-03-14 00:58:49 +0900

    feat : JPA 사용하지 않기

--- src/main/java/codesquad/domain/User.java
+++ src/main/java/codesquad/domain/User.java
@@ -1,15 +1,6 @@
 package codesquad.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
 public class User {
-
-    @Id
-    @GeneratedValue
-    private Long id;
 
     private String userId;
 
@@ -18,13 +9,6 @@ public class User {
     private String name;
 
     private String email;
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
 
     public String getUserId() {
         return userId;
@@ -40,10 +24,6 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public void setUserId(String userId) {
