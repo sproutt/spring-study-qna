f625c0c4187a859fde19f466fdcd6f2de8e60c88
saint6839 <saint6839@gmail.com>
2022-03-14 10:53:48 +0900

    refactor: @Entity 어노테이션 제거

--- src/main/java/codesquad/domain/User.java
+++ src/main/java/codesquad/domain/User.java
@@ -1,33 +1,22 @@
 package codesquad.domain;
 
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userId;
     private String password;
     private String name;
     private String email;
 
-    public User(String userId, String password, String name, String email) {
+    public User(Long id, String userId, String password, String name, String email) {
+        this.id = id;
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
     }
 
-    protected User() {
-    }
-
-    public static User craeteUser(String userId, String password, String name, String email) {
-        return new User(userId, password, name, email);
+    public void createId(Long id) {
+        this.id = id;
     }
 
     public Long getId() {
