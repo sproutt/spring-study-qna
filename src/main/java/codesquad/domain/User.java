8734793ae1470c0a698eeb69da9a0decc41bfe13
kanamycine <jinyeong5320@gmail.com>
2022-03-15 01:22:00 +0900

    feat : DB 없이 회원가입 , 목록 기능 구현

---
+++ src/main/java/codesquad/domain/User.java
@@ -0,0 +1,50 @@
+package codesquad.domain;
+
+public class User {
+	private long id;
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
+
