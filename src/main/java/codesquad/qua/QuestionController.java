c1e55ae77f899f2ab5157c6abb77d3d37c0eefbb
hyeonwoo <gusdn85554@naver.com>
2022-03-15 09:13:23 +0900

    refactor : 패키지 구조 변경
    domain과 controller 별로 나누지 않고 UserController와 User는 user 패키지에 저장

---
+++ src/main/java/codesquad/qua/QuestionController.java
@@ -0,0 +1,8 @@
+package codesquad.qua;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QuestionController {
+
+}
--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -1,7 +1,7 @@
-package codesquad.controller;
+package codesquad.user;
 
 
-import codesquad.domain.User;
+import codesquad.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
