93c25cea96db1a59dc4c95e4f33622cabfd17b3c
hyeonwoo <gusdn85554@naver.com>
2022-03-15 10:23:20 +0900

    feat : 질문하기, 질문목록, 질문 상세보기 기능 구현
    질문목록을 보기 위한 list.html은 user의 list.html을 참고함
    
    상세보기 기능 구현 시 index가 1부터 시작하여 @index_1 사용

--- src/main/java/codesquad/qua/QuestionController.java
+++ src/main/java/codesquad/qua/QuestionController.java
@@ -1,8 +1,36 @@
 package codesquad.qua;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class QuestionController {
 
+    private final List<Question> registry = new ArrayList<>();
+
+    @PostMapping("/questions")
+    public String ask(Question question) {
+        registry.add(question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String list(Model model) {
+        model.addAttribute("question", registry);
+        return "qna/list";
+    }
+
+    @GetMapping("/questions/{index}")
+    public String qnaList(Model model, @PathVariable int index) {
+        Question question = registry.get(index - 1);
+        model.addAttribute("qna", question);
+
+        return "qna/show";
+    }
 }
