5d9cb00fdaa5ebb044fa5a7d6b64dc27b73f9059
saint6839 <saint6839@gmail.com>
2022-03-17 11:15:45 +0900

    refactor: 예외 메시지 수정

--- src/main/java/codesquad/exception/NoSuchQuestionException.java
+++ src/main/java/codesquad/exception/NoSuchQuestionException.java
@@ -3,7 +3,7 @@ package codesquad.exception;
 import java.util.NoSuchElementException;
 
 public class NoSuchQuestionException extends NoSuchElementException {
-    private static final String NOT_EXIST_QUESTION_ID_ERROR = "해당 사용자 아이디와 일치하는 사용자가 없습니다.";
+    private static final String NOT_EXIST_QUESTION_ID_ERROR = "해당하는 질문을 찾을 수 없습니다.";
 
     public NoSuchQuestionException() {
         super(NOT_EXIST_QUESTION_ID_ERROR);
