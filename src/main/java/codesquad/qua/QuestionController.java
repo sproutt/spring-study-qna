db6d51cf6850f855d8033330f7b8e993fb7ea3c5
saint6839 <saint6839@gmail.com>
2022-03-24 22:38:30 +0900

    feat : QuestionController DB 연동
    -create() : 질문 생성
    -list() : 질문 목록 조회
    -qnaInfo() : 질문 조회
    -update() : 질문 수정
    -updateForm() : 질문 수정 폼 불러오기
    -remove() : 질문 삭제

--- src/main/java/codesquad/qua/QuestionController.java
+++ src/main/java/codesquad/qua/QuestionController.java
@@ -1,36 +1,57 @@
 package codesquad.qua;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class QuestionController {
 
-    private final List<Question> registry = new ArrayList<>();
+    @Autowired
+    QuestionRepository questionRepository;
 
     @PostMapping("/questions")
-    public String ask(Question question) {
-        registry.add(question);
+    public String create(Question question) {
+        questionRepository.save(question);
         return "redirect:/";
     }
 
     @GetMapping("/")
     public String list(Model model) {
-        model.addAttribute("question", registry);
+        model.addAttribute("question", questionRepository.findAll());
         return "qna/list";
     }
 
-    @GetMapping("/questions/{index}")
-    public String qnaList(Model model, @PathVariable("index") int index) {
-        Question question = registry.get(index - 1);
-        model.addAttribute("qna", question);
-
+    @GetMapping("/questions/{id}")
+    public String qnaInfo(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("question", questionRepository.findById(id).get());
         return "qna/show";
+    }
+
+    @PostMapping("/questions/{id}/update")
+    public String update(Question question, @PathVariable("id") Long id) {
+        Question findQuestion = questionRepository.findById(id).get();
+
+        findQuestion.update(question);
+        questionRepository.save(findQuestion);
+        return "redirect:/";
+    }
+
+    @GetMapping("/questions/{id}/updateForm")
+    public String updateForm(Model model, @PathVariable("id") Long id) {
+        Question findQuestion = questionRepository.findById(id).get();
+        model.addAttribute("question", findQuestion);
+
+        return "/qna/updateForm";
+    }
+
+    @DeleteMapping("/questions/{id}/delete")
+    public String remove(@PathVariable("id") Long id) {
+        questionRepository.delete(questionRepository.findById(id).get());
+        return "redirect:/";
     }
 }
