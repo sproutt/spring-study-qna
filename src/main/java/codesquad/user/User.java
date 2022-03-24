a4a4cc801c7ee9cf7231ebf80cfdda5480e7f075
saint6839 <saint6839@gmail.com>
2022-03-24 22:28:14 +0900

    feat : id 필드, get 메서드 추가

--- src/main/java/codesquad/user/User.java
+++ src/main/java/codesquad/user/User.java
@@ -1,7 +1,16 @@
 package codesquad.user;
 
+
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
     private String userId;
 
     private String password;
@@ -40,6 +49,10 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void update(User user) {
