7c5a2284a3ddefbf07d54620850cc336e626c292
kanamycine <jinyeong5320@gmail.com>
2022-03-25 20:55:00 +0900

    feat : User 클래스 DB Table에 매핑

--- src/main/java/codesquad/domain/User.java
+++ src/main/java/codesquad/domain/User.java
@@ -1,8 +1,20 @@
 package codesquad.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable=false, length=20)
 	private String userId;
+
 	private String password;
 	private String name;
 	private String email;
