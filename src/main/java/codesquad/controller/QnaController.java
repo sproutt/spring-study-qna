8b260de42fe0ec2cd1bdbd36e1ed21e674dd3e3f
kanamycine <jinyeong5320@gmail.com>
2022-03-25 17:04:58 +0900

    fix : qnaController의 list 메서드 GET Mapping -> RequestMapping 으로 변경

--- src/main/java/codesquad/controller/QnaController.java
+++ src/main/java/codesquad/controller/QnaController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import codesquad.domain.Question;
 import codesquad.domain.User;
@@ -30,7 +32,8 @@ public class QnaController {
 		return "redirect:/";
 	}
 
-	@GetMapping("/")
+	// @GetMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String list(Model model){
 		model.addAttribute("questions", questions);
 		return "index";
