9a4743582aa2bcdadacde2a8d5d34d446d80bdbf
sunghyuki <sunghyuk1609@gmail.com>
2022-03-27 14:22:48 +0900

    refactor : User 클래스를 DB 테이블에 매핑

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -1,6 +1,6 @@
 package codesquad.web;
 
-import codesquad.domain.User;
+import codesquad.domain.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
