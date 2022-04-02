ded14d1b249cfbd42265811fde1d8cdf57bf97cf
saint6839 <saint6839@gmail.com>
2022-04-02 18:00:57 +0900

    refactor: JpaRepository로 변경

--- src/main/java/codesquad/repository/UserRepository.java
+++ src/main/java/codesquad/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package codesquad.repository;
 
 import codesquad.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
 }
