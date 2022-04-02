91ea5fab8d9fa049d6757304570e62ddcf66d96f
saint6839 <saint6839@gmail.com>
2022-04-02 22:43:59 +0900

    refactor: 쿼리문으로 생성한 더미데이터로 로그인 테스트 가능하도록 수정

--- src/test/java/codesquad/controller/UserControllerTest.java
+++ src/test/java/codesquad/controller/UserControllerTest.java
@@ -29,62 +29,62 @@ class UserControllerTest {
     @DisplayName("한명의 사용자가 회원가입 했을때 사용자가 잘 조회되는지 테스트")
     void 회원가입() {
         // given
-        User user1 = new User("user1", "1", "채상엽", "email1@gmail.com");
+        User user3 = new User("user3", "1", "채상엽", "email1@gmail.com");
 
         // when
-        userRepository.save(user1);
+        userRepository.save(user3);
 
         // then
-        User result = userRepository.findByUserId("user1")
+        User result = userRepository.findByUserId("user3")
                                     .get();
 
-        assertThat(user1).isEqualTo(result);
+        assertThat(user3).isEqualTo(result);
     }
 
     @Test
     @DisplayName("여러명의 사용자가 회원가입 했을때 사용자 목록의 크기가 일치하는지 테스트")
     void 회원목록_조회() {
         // given
-        User user1 = new User("user1", "1", "채상엽", "email1@gmail.com");
-        User user2 = new User("user2", "1", "홍동건", "email2@gmail.com");
-        User user3 = new User("user3", "1", "김성혁", "email3@gmail.com");
-        User user4 = new User("user3", "1", "김현우", "email4@gmail.com");
+        User user3 = new User("user3", "1", "채상엽", "email1@gmail.com");
+        User user4 = new User("user4", "1", "홍동건", "email2@gmail.com");
+        User user5 = new User("user5", "1", "김성혁", "email3@gmail.com");
+        User user6 = new User("user6", "1", "김현우", "email4@gmail.com");
 
         // when
-        userRepository.save(user1);
-        userRepository.save(user2);
         userRepository.save(user3);
         userRepository.save(user4);
+        userRepository.save(user5);
+        userRepository.save(user6);
 
         // then
         List<User> results = userRepository.findAll();
-        assertThat(results.size()).isEqualTo(4);
+        assertThat(results.size()).isEqualTo(6);
     }
 
     @Test
     @DisplayName("회원 정보를 수정했을때 수정 내용이 잘 반영되는지 테스트")
     void 회원정보_수정() {
         // given
-        User user1 = new User("user1", "1", "채상엽", "email1@gmail.com");
+        User user3 = new User("user3", "1", "채상엽", "email1@gmail.com");
         EditUserDto editUserDto = new EditUserDto("1", "홍동건", "email2@gmail.com");
 
         // when
-        user1.update(editUserDto);
+        user3.update(editUserDto);
 
         // then
-        assertThat(user1.getName()).isEqualTo("홍동건");
+        assertThat(user3.getName()).isEqualTo("홍동건");
     }
 
     @Test
     @DisplayName("로그인 했을때 세션에 사용자 정보가 잘 저장 되는지 테스트")
     void 로그인() {
         // given
-        User user1 = new User("user1", "1", "채상엽", "email1@gmail.com");
+        User user1 = userRepository.findByUserId("user1")
+                                   .get();
         MockHttpSession session = new MockHttpSession();
         session.setAttribute("user1", user1);
 
-        // when
-        userRepository.save(user1);
+        // when®
         userController.login("user1", "1", session);
 
         // then
