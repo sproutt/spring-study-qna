b1374d440047cd4ec4fbabb85d2eb4de3e25b521
saint6839 <saint6839@gmail.com>
2022-04-02 22:01:13 +0900

    feat: 작가명으로 조회하는 메서드 추가

--- src/main/java/codesquad/repository/QuestionRepository.java
+++ src/main/java/codesquad/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package codesquad.repository;
 import codesquad.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findByWriter(String writer);
 }
