0cbddfbf511be83cc91ea28a29927b71f51dd260
sunghyuki <sunghyuk1609@gmail.com>
2022-04-03 09:32:41 +0900

    feat : SLF4J를 이용한 로깅 추가 - 롬봄 의존성 추가 - 유저 생성 시 유저 객체를 로그를 통해 확인

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -2,6 +2,7 @@ package codesquad.web;
 
 import codesquad.domain.user.User;
 import codesquad.domain.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.NoSuchElementException;
 
 @Controller
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -25,7 +27,7 @@ public class UserController {
 
     @PostMapping("/users")
     public String create(User user) {
-        System.out.println("user = " + user);
+        log.info("user = {}", user);
         userRepository.save(user);
         return "redirect:/users";
     }
