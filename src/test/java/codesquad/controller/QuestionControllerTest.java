578bf8d8b71d98a988d7ad02b853e84c614e7436
saint6839 <saint6839@gmail.com>
2022-04-04 16:37:44 +0900

    refactor: 쿼리문으로 생성한 더미데이터 반영해서 테스트 코드 수정

--- src/test/java/codesquad/controller/QuestionControllerTest.java
+++ src/test/java/codesquad/controller/QuestionControllerTest.java
@@ -1,7 +1,11 @@
 package codesquad.controller;
 
 import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.domain.WriteQuestionDto;
 import codesquad.repository.QuestionRepository;
+import codesquad.repository.UserRepository;
+import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +20,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 class QuestionControllerTest {
-
     @Autowired
     private QuestionRepository questionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     @DisplayName("질문을 등록했을때 질문이 잘 조회되는지 테스트")
     void 질문_등록() {
         // given
+        User user1 = userRepository.findById(1L)
+                                  .get();
+
         LocalDateTime localDateTime = LocalDateTime.now();
-        Question question = new Question("writer1", "title1", "content1", localDateTime);
+        Question question = new Question(user1, "title1", "content1", localDateTime);
 
         // when
         questionRepository.save(question);
 
         // then
-        Question result = questionRepository.findByWriter("writer1")
+        Question result = questionRepository.findByWriter(user1)
                                             .get();
         assertThat(question).isEqualTo(result);
     }
@@ -40,33 +49,61 @@ class QuestionControllerTest {
     @DisplayName("질문이 여러개가 등록되었을때 질문 목록 크기가 일치하는지 테스트")
     void 질문목록_조회() {
         // given
+        User user1 = userRepository.findById(1L)
+                                   .get();
+        User user2 = userRepository.findById(2L)
+                                   .get();
+
         LocalDateTime localDateTime = LocalDateTime.now();
-        Question question = new Question("writer1", "title1", "content1", localDateTime);
-        Question question2 = new Question("writer2", "title2", "content2", localDateTime);
-        Question question3 = new Question("writer3", "title3", "content3", localDateTime);
-        Question question4 = new Question("writer4", "title4", "content4", localDateTime);
+        Question question1 = new Question(user1, "title1", "content1", localDateTime);
+        Question question2 = new Question(user2, "title2", "content2", localDateTime);
 
         // when
-        questionRepository.save(question);
+        questionRepository.save(question1);
         questionRepository.save(question2);
-        questionRepository.save(question3);
-        questionRepository.save(question4);
 
         // then
         List<Question> result = questionRepository.findAll();
-        assertThat(result.size()).isEqualTo(4);
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("등록된 질문을 수정하였을때 내용이 잘 수정되는지 확인")
+    void 질문_수정() {
+        // given
+        User user1 = userRepository.findById(1L)
+                                   .get();
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Question question1 = new Question(user1, "title1", "content1", localDateTime);
+        WriteQuestionDto writeQuestionDto = new WriteQuestionDto("수정된 제목", "수정된 내용");
+
+        // when
+        questionRepository.save(question1);
+        Question question = questionRepository.findByWriter(user1)
+                                            .get();
+        question.write(user1, writeQuestionDto, localDateTime);
+        questionRepository.save(question);
+
+        // then
+        Question result = questionRepository.findByWriter(user1)
+                                               .get();
+        assertThat(result.getTitle()).isEqualTo(question.getTitle());
     }
 
     @Test
     @DisplayName("질문을 삭제했을때 정상적으로 삭제가 되는지 테스트")
     void 질문_삭제() {
         // given
+        User user1 = userRepository.findById(2L)
+                                  .get();
+
         LocalDateTime localDateTime = LocalDateTime.now();
-        Question question = new Question("writer1", "title1", "content1", localDateTime);
+        Question question = new Question(user1, "title1", "content1", localDateTime);
 
         // when
         questionRepository.save(question);
-        questionRepository.delete(questionRepository.findByWriter("writer1")
+        questionRepository.delete(questionRepository.findByWriter(user1)
                                                     .get());
         // then
         List<Question> result = questionRepository.findAll();
