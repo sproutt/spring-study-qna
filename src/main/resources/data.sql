8bdad9b8af97cc4e135c610f92a7366db1069bf7
kanamycine <jinyeong5320@gmail.com>
2022-04-06 00:08:42 +0900

    feat : test data 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (id, user_id, password, name, email) VALUES (1, '1234', '1234', '자바지기', 'javajigi@slipp.net');
+INSERT INTO USER (id, user_id, password, name, email) VALUES (2, 'sanjigi', 'test', '산지기', 'sanjigi@slipp.net');
