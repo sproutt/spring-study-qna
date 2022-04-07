133e485351d8e4def53963dcda2c601d69bd4852
saint6839 <saint6839@gmail.com>
2022-04-07 15:42:26 +0900

    refactor: session값 변수명 이름 수정, 수정폼 인증로직 추가, session 가져오는 기능 메서드 분리

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -3,8 +3,6 @@ package codesquad.controller;
 import codesquad.domain.Question;
 import codesquad.domain.User;
 import codesquad.domain.WriteQuestionDto;
-import codesquad.exception.NoSuchQuestionException;
-import codesquad.exception.NoSuchUserException;
 import codesquad.repository.QuestionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @Controller
 public class QuestionController {
@@ -24,8 +23,8 @@ public class QuestionController {
 
     @GetMapping("/questions/form")
     public String showQuestionForm(HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
+        Object sessionValue = getSessionValue(session);
+        if (sessionValue != null) {
             return "qna/form";
         }
         return "qna/no_session_form";
@@ -41,25 +40,38 @@ public class QuestionController {
     public String showQuestionDetail(@PathVariable Long id, Model model) {
         try {
             Question question = questionRepository.findById(id)
-                                                  .orElseThrow(NoSuchQuestionException::new);
+                                                  .orElseThrow(NoSuchElementException::new);
             model.addAttribute("question", question);
-        } catch (NoSuchQuestionException exception) {
+        } catch (NoSuchElementException exception) {
             return "qna/no_question_form";
         }
         return "qna/show";
     }
 
     @GetMapping("/questions/{id}/editor")
-    public String showEditForm() {
-        return "qna/edit_form";
+    public String showEditForm(@PathVariable Long id, HttpSession session) {
+        Object sessionValue = getSessionValue(session);
+        if (sessionValue != null) {
+            try {
+                User sessionedUser = (User) sessionValue;
+                questionRepository.findById(id)
+                                  .filter(s -> s.getId()
+                                                .equals(sessionedUser.getId()))
+                                  .orElseThrow(NoSuchElementException::new);
+            } catch (NoSuchElementException exception) {
+                return "qna/no_access_form";
+            }
+            return "qna/edit_form";
+        }
+        return "qna/no_session_form";
     }
 
     @PostMapping("/questions")
     public String create(@ModelAttribute WriteQuestionDto writeQuestionDto, HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
+        Object sessionValue = getSessionValue(session);
+        if (sessionValue != null) {
             Question question = new Question();
-            User sessionedUser = (User) value;
+            User sessionedUser = (User) sessionValue;
             question.write(sessionedUser, writeQuestionDto, getWrittenTime());
             questionRepository.save(question);
             return "redirect:/questions";
@@ -73,40 +85,44 @@ public class QuestionController {
 
     @PutMapping("/questions/{id}")
     public String edit(@PathVariable Long id, WriteQuestionDto writeQuestionDto, HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        // 작성 부분으로 이동시키고 작성해서 다시 돌아오게 만들어야함
-        if (value != null) {
+        Object sessionValue = getSessionValue(session);
+        if (getSessionValue(session) != null) {
             try {
-                User sessionedUser = (User) value;
+                User sessionedUser = (User) sessionValue;
                 Question question = questionRepository.findById(id)
                                                       .filter(s -> s.getId()
                                                                     .equals(sessionedUser.getId()))
-                                                      .orElseThrow(NoSuchUserException::new
+                                                      .orElseThrow(NoSuchElementException::new
                                                       );
 
                 question.write(sessionedUser, writeQuestionDto, getWrittenTime());
                 questionRepository.save(question);
-            } catch (NoSuchUserException exception) {
-                return "qna/no_access_form";
+            } catch (NoSuchElementException exception) {
+                return "qna/no_session_form";
+            }
+            return "redirect:/questions";
+        }
+        return "qna/no_access_form";
+    }
+
+    @DeleteMapping("/questions/{id}")
+    public String remove(@PathVariable Long id, HttpSession session) {
+        Object sessionValue = getSessionValue(session);
+        if (getSessionValue(session) != null) {
+            try {
+                User sessionedUser = (User) sessionValue;
+                questionRepository.delete(questionRepository.findById(id)
+                                                            .filter(s -> s.getId()
+                                                                          .equals(sessionedUser.getId()))
+                                                            .orElseThrow(NoSuchElementException::new));
+            } catch (NoSuchElementException exception) {
+                return "redirect:/users/login";
             }
         }
         return "redirect:/questions";
     }
 
-    @DeleteMapping("/questions/{id}")
-    public String remove(@PathVariable Long id, HttpSession session) {
-        Object value = session.getAttribute("sessionedUser");
-        if (value != null) {
-            try {
-                User sessionedUser = (User) value;
-                questionRepository.delete(questionRepository.findById(id)
-                                                            .filter(s -> s.getId()
-                                                                          .equals(sessionedUser.getId()))
-                                                            .orElseThrow(NoSuchQuestionException::new));
-            } catch (NoSuchQuestionException exception) {
-                return "redirect:/users/login";
-            }
-        }
-        return "redirect:/questions";
+    private Object getSessionValue(HttpSession session) {
+        return session.getAttribute("sessionedUser");
     }
 }
