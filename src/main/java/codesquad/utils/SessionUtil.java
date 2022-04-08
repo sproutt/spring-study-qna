d6647f9aad47fc04f02ae27bd16e42b882d2a455
hyeonwoo <gusdn85554@naver.com>
2022-04-08 16:30:42 +0900

    상수명 변경

--- src/main/java/codesquad/utils/SessionUtil.java
+++ src/main/java/codesquad/utils/SessionUtil.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpSession;
 
 public class SessionUtil {
 
-    private static final String SESSION_NAME = "sessionedUser";
+    private static final String SESSION_KEY = "sessionedUser";
 
     public static User getUserBySession(HttpSession session) {
-        return (User) session.getAttribute(SESSION_NAME);
+        return (User) session.getAttribute(SESSION_KEY);
     }
 
     public static void setSession(HttpSession session, User user) {
-        session.setAttribute(SESSION_NAME, user);
+        session.setAttribute(SESSION_KEY, user);
     }
 }
