eb9d108ae83358dcaf75019ec311c6eaa9b4a2aa
sunghyuki <sunghyuk1609@gmail.com>
2022-04-15 17:11:20 +0900

    feat : UserController 컨트롤러 메서드 Exception 핸들링 - alert 알림창에 에러 내용 표시

--- src/main/java/codesquad/web/UserController.java
+++ src/main/java/codesquad/web/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.NoSuchElementException;
@@ -54,58 +55,76 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}/form")
-    public String userInfoUpdateForm(@PathVariable Long id, Model model, HttpSession httpSession) {
-        if (!HttpSessionUtil.isLoginUser(httpSession)) {
+    public String userInfoUpdateForm(@PathVariable Long id, Model model, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        try {
+            if (!HttpSessionUtil.isLoginUser(httpSession)) {
+                throw new IllegalStateException("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            }
+
+            User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
+
+            if (!sessionedUser.equalsId(id)) {
+                httpSession.removeAttribute(HttpSessionUtil.SESSION_KEY);
+                throw new IllegalStateException("다른 사람의 개인정보를 수정할 수 없습니다. 다시 로그인 해주세요.");
+            }
+
+            model.addAttribute("user", sessionedUser);
+
+        } catch(IllegalStateException exception) {
+            redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
             return "redirect:/users/login/form";
         }
-
-        User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
-
-        if (!sessionedUser.equalsId(id)) {
-            return "failed";
-        }
-
-        model.addAttribute("user", sessionedUser);
 
         return "user/updateForm";
     }
 
     @PostMapping("/users/{id}/update")
-    public String update(@PathVariable Long id, User updatedUser, HttpSession httpSession) {
-        if (!HttpSessionUtil.isLoginUser(httpSession)) {
+    public String update(@PathVariable Long id, User updatedUser, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        try {
+            if (!HttpSessionUtil.isLoginUser(httpSession)) {
+                throw new IllegalStateException("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            }
+
+            User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
+
+            if (!sessionedUser.equalsId(id)) {
+                throw new IllegalStateException("아이디가 일치하지 않습니다.");
+            }
+
+            if (!sessionedUser.equalsPassword(updatedUser.getPassword())) {
+                throw new IllegalStateException("비밀번호가 일치하지 않습니다.");
+            }
+
+            sessionedUser.update(updatedUser);
+
+            userRepository.save(sessionedUser);
+        } catch(IllegalStateException exception) {
+            redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
             return "redirect:/users/login/form";
         }
-
-        User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
-
-        if (!sessionedUser.equalsId(id)) {
-            return "failed";
-        }
-
-        if (!sessionedUser.equalsPassword(updatedUser.getPassword())) {
-            return "failed";
-        }
-
-        sessionedUser.update(updatedUser);
-        userRepository.save(sessionedUser);
 
         return "redirect:/users";
     }
 
 
     @PostMapping("/login")
-    public String login(String userId, String password, HttpSession httpSession) {
-        User savedUser = userRepository.findByUserId(userId);
+    public String login(String userId, String password, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        try {
+            User savedUser = userRepository.findByUserId(userId);
 
-        if (savedUser == null) {
+            if (savedUser == null) {
+                throw new IllegalStateException("아이디와 패스워드를 다시 확인해주세요.");
+            }
+
+            if (!savedUser.equalsPassword(password)) {
+                throw new IllegalStateException("패스워드가 일치하지 않습니다.");
+            }
+
+            httpSession.setAttribute("sessionedUser", savedUser);
+        } catch (IllegalStateException exception) {
+            redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
             return ":redirect/users/login/form";
         }
-
-        if (!savedUser.equalsPassword(password)) {
-            return ":redirect/users/login/form";
-        }
-
-        httpSession.setAttribute("sessionedUser", savedUser);
         return "redirect:/";
     }
 }
