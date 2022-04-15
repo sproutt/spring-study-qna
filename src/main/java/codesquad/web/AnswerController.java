c51bca5e60836031d27a4d986846c5eaf1b0bad5
sunghyuki <sunghyuk1609@gmail.com>
2022-04-15 17:49:29 +0900

    feat : AnswerController 컨트롤러 메서드 Exception 핸들링 - alert 알림창에 에러 내용 표시

--- src/main/java/codesquad/web/AnswerController.java
+++ src/main/java/codesquad/web/AnswerController.java
@@ -8,11 +8,11 @@ import codesquad.domain.user.User;
 import codesquad.util.HttpSessionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.NoSuchElementException;
@@ -28,44 +28,58 @@ public class AnswerController {
     private AnswerRepository answerRepository;
 
     @PostMapping("")
-    public String create(@PathVariable Long index, Answer answer, HttpSession httpSession, Model model) {
-        if (!HttpSessionUtil.isLoginUser(httpSession)) {
+    public String create(@PathVariable Long index, Answer answer, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        try {
+            if (!HttpSessionUtil.isLoginUser(httpSession)) {
+                throw new IllegalStateException("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            }
+
+            User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
+            Question savedQuestion = questionRepository.findById(index).orElseThrow(() -> new NoSuchElementException("게시글이 존재하지 않습니다."));
+
+            answer.setWriter(sessionedUser);
+            answer.setQuestion(savedQuestion);
+            savedQuestion.addCountOfAnswer();
+
+            answerRepository.save(answer);
+        } catch (IllegalStateException exception) {
+            redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
             return "redirect:/users/login/form";
+        } catch (NoSuchElementException exception) {
+            return "redirect:/";
         }
-
-        User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
-        Question savedQuestion = questionRepository.findById(index).orElseThrow(NoSuchElementException::new);
-
-        answer.setWriter(sessionedUser);
-        answer.setQuestion(savedQuestion);
-        savedQuestion.addCountOfAnswer();
-
-        answerRepository.save(answer);
 
         return String.format("redirect:/questions/%d", index);
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long index, @PathVariable Long id, HttpSession httpSession) {
-        if (!HttpSessionUtil.isLoginUser(httpSession)) {
+    public String delete(@PathVariable Long index, @PathVariable Long id, HttpSession httpSession, RedirectAttributes redirectAttributes) {
+        try {
+            if (!HttpSessionUtil.isLoginUser(httpSession)) {
+                throw new IllegalStateException("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            }
+
+            Answer savedAnswer = answerRepository.findById(id).orElseThrow(() -> new NoSuchElementException("답변이 존재하지 않습니다."));
+            User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
+
+            if (!savedAnswer.isSameWriter(sessionedUser)) {
+                throw new IllegalStateException("다른 사람의 게시글을 삭제할 수 없습니다.");
+            }
+
+            savedAnswer.delete();
+
+            answerRepository.save(savedAnswer);
+
+            Question savedQuestion = questionRepository.findById(index).orElseThrow(() -> new NoSuchElementException("게시글이 존재하지 않습니다."));
+            savedQuestion.deleteAnswer();
+
+            questionRepository.save(savedQuestion);
+        } catch (IllegalStateException exception) {
+            redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
             return "redirect:/users/login/form";
+        } catch(NoSuchElementException exception) {
+            return "redirect:/";
         }
-
-        Answer savedAnswer = answerRepository.findById(id).orElseThrow(NoSuchElementException::new);
-        User sessionedUser = HttpSessionUtil.getUserFrom(httpSession);
-
-        if (!savedAnswer.isSameWriter(sessionedUser)) {
-            return "redirect:/users/login/form";
-        }
-
-        savedAnswer.delete();
-
-        answerRepository.save(savedAnswer);
-
-        Question savedQuestion = questionRepository.findById(index).orElseThrow(NoSuchElementException::new);
-        savedQuestion.deleteAnswer();
-
-        questionRepository.save(savedQuestion);
 
         return String.format("redirect:/questions/{index}");
     }
