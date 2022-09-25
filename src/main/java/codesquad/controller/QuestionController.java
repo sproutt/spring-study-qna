92f9a9be780c686e16f1815703883d4c6ec666a6
mywnajsldkf <mywnajsldkf@gmail.com>
2022-09-25 22:17:21 +0900

    feat: 질문 등록 기능 구현

--- src/main/java/codesquad/controller/QuestionController.java
+++ src/main/java/codesquad/controller/QuestionController.java
@@ -1,12 +1,33 @@
 package codesquad.controller;
 
+import codesquad.AppConfig;
+import codesquad.dto.question.QuestionRequestDto;
+import codesquad.entity.QuestionEntity;
+import codesquad.mapper.QuestionMapper;
+import codesquad.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    QuestionService questionService = applicationContext.getBean("questionService", QuestionService.class);
+
     @GetMapping("/question/post")
     public String getQuestionForm() {
         return "qna/form";
+    }
+
+    @PostMapping("/question")
+    public String postQuestion(QuestionRequestDto questionRequestDto) {
+        QuestionEntity questionEntity = QuestionMapper.questionMapper.dtoToEntity(questionRequestDto);
+        questionService.postQuestion(questionEntity);
+
+        return "redirect:/";
     }
 }
