45154302ffea8a1906a794880d3f084d971ccf8d
mywnajsldkf <mywnajsldkf@gmail.com>
2022-09-25 22:12:29 +0900

    feat: 질문 서비스 생성자 주입

--- src/main/java/codesquad/AppConfig.java
+++ src/main/java/codesquad/AppConfig.java
@@ -1,7 +1,11 @@
 package codesquad;
 
+import codesquad.repository.ArrayQuestionRepository;
 import codesquad.repository.ArrayUserRepository;
+import codesquad.repository.QuestionRepository;
 import codesquad.repository.UserRepository;
+import codesquad.service.QuestionService;
+import codesquad.service.QuestionServiceImpl;
 import codesquad.service.UserService;
 import codesquad.service.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
+    
     @Bean
     public UserService userService() {
         return new UserServiceImpl(userRepository());
@@ -18,5 +22,15 @@ public class AppConfig {
     @Bean
     public UserRepository userRepository() {
         return new ArrayUserRepository();
+    }
+
+    @Bean
+    public QuestionService questionService() {
+        return new QuestionServiceImpl(questionRepository());
+    }
+
+    @Bean
+    public QuestionRepository questionRepository() {
+        return new ArrayQuestionRepository();
     }
 }
